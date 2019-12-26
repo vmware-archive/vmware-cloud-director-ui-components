cdaa759380126c38e2fa3811b45a5e1973baa52b
juanmendes <mendesjuan@gmail.com>
2019-12-26 16:04:25 -0500

    Data Exporter initial implementation (#8)
    * Data Exporter initial implementation
    
    Does not include unit tests
    
    Signed-off-by: Juan Mendes <jmendes@vmware.com>

--- projects/examples/src/app/app.module.ts
+++ projects/examples/src/app/app.module.ts
@@ -8,15 +8,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsModule } from '@vmw/vcd-ui-components';
+import { DataExporterModule } from '@vmw/vcd-ui-components';
 import { DocLibModule } from '@vmw/vcd-ui-doc-lib';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, ComponentsModule, DocLibModule, ClarityModule, BrowserAnimationsModule],
-    providers: [],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        DataExporterModule,
+        DocLibModule,
+        ClarityModule,
+        BrowserAnimationsModule,
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
