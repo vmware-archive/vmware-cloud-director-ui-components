f18c81dc9e8767b05d1588562806888a18a76f21
Juan Mendes <jmendes@vmware.com>
2020-03-26 17:24:36 -0400

    Fixing unit tests
    Signed-off-by: Juan Mendes <jmendes@vmware.com>

--- projects/examples/src/polyfills.ts
+++ projects/examples/src/polyfills.ts
@@ -88,9 +88,3 @@ import 'core-js/es7/reflect';
 if (!Element.prototype.matches) {
     Element.prototype.matches = (Element.prototype as any).msMatchesSelector || Element.prototype.webkitMatchesSelector;
 }
-
-Number.isInteger =
-    Number.isInteger ||
-    function(value) {
-        return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
-    };
