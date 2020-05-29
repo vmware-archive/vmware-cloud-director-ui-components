a72e3c1aa949d52b464685083fc0a3a794d3a8ff
Juan Mendes <jmendes@vmware.com>
2020-05-29 12:14:53 -0400

    Removed reference to non-existing home.component.css
    Signed-off-by: Juan Mendes <jmendes@vmware.com>

--- projects/examples/src/app/home/home.component.ts
+++ projects/examples/src/app/home/home.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
     selector: 'vcd-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
     ghReadMeUrl =
