b462dde4426f5ea96851ac77ed5bf2aea390d1e5
ps37 <prudhvi.af121@gmail.com>
2020-11-06 15:10:26 -0500

    Replace DropdownItem with ActionMenuItem
    Signed-off-by: ps37 <prudhvi.af121@gmail.com>

--- projects/components/src/dropdown/dropdown.component.ts
+++ projects/components/src/dropdown/dropdown.component.ts
@@ -16,46 +16,13 @@ import {
     ViewChildren,
 } from '@angular/core';
 import { ClrDropdown } from '@clr/angular';
-import { TextIcon } from '../common/interfaces';
+import { ActionItem, TextIcon } from '../common/interfaces';
 import { CliptextConfig, TooltipSize } from '../lib/directives';
+import { CommonUtil } from '../utils';
 
 const NESTED_DROPDOWN_TRIGGER_SELECTOR = 'clr-dropdown clr-dropdown > button';
 const DROPDOWN_ITEM_SELECTOR = 'clr-dropdown-menu > button';
 export const NESTED_MENU_HIDE_DELAY = 400;
-
-/**
- * Object representing an item of the dropdown
- */
-export interface DropdownItem<T extends DropdownItem<T>> {
-    /**
-     * The i18n key or a translated string for contents of a action button
-     */
-    textKey?: string;
-    /**
-     * List of items that will be grouped under this item
-     */
-    children?: T[];
-    /**
-     * The Clarity icon of the contextual button that is displayed if the button is featured.
-     */
-    icon?: string;
-    /**
-     * To mark if the {@link #ActionItem.textKey} has to be translated or not
-     */
-    isTranslatable?: boolean;
-    /**
-     * CSS class of the dropdown item. Must be unique among all dropdown items within the dropdown items list
-     */
-    class?: string;
-    /**
-     * To add separators between groups of dropdown items
-     */
-    isSeparator?: boolean;
-    /**
-     * This is the busy state for the menu item. Used by extension actions of plugins
-     */
-    busy?: boolean;
-}
 
 /**
  * Dumb component used for displaying nested drop downs
@@ -65,12 +32,10 @@ export interface DropdownItem<T extends DropdownItem<T>> {
     templateUrl: 'dropdown.component.html',
     styleUrls: ['./dropdown.component.scss'],
 })
-export class DropdownComponent<T extends DropdownItem<T>> {
-    private _onItemClickedCb: (item: T) => void;
-
+export class DropdownComponent {
     private _dropdownItemContents: TextIcon = TextIcon.TEXT;
 
-    private _items: T[];
+    private _items: ActionItem<unknown, unknown>[];
 
     /**
      * Decides what goes into the action buttons
@@ -87,14 +52,14 @@ export class DropdownComponent<T extends DropdownItem<T>> {
     /**
      * Nested list of dropdown objects
      */
-    @Input() set items(items: T[]) {
+    @Input() set items(items: ActionItem<unknown, unknown>[]) {
         this._items = this.flattenNestedItemsWithSingleChild(items);
     }
-    get items(): T[] {
+    get items(): ActionItem<unknown, unknown>[] {
         return this._items;
     }
 
-    constructor(@Optional() @SkipSelf() @Host() private parentVcdDropdown: DropdownComponent<T>) {}
+    constructor(@Optional() @SkipSelf() @Host() private parentVcdDropdown: DropdownComponent) {}
     /**
      * If a icon should be displayed inside contextual buttons
      */
@@ -133,7 +98,7 @@ export class DropdownComponent<T extends DropdownItem<T>> {
      * The {@link ngForTrackBy} method used for rendering of a dropdown actions or for rendering nested drop downs
      * NOTE: Without this, nested drop downs don't get rendered on the screen
      */
-    @Input() trackByFunction: TrackByFunction<T>;
+    @Input() trackByFunction: TrackByFunction<ActionItem<unknown, unknown>>;
 
     /**
      * The position the root dropdown with respect to root dropdown trigger button. Refer to {@link clrPosition} for it's values
@@ -146,21 +111,10 @@ export class DropdownComponent<T extends DropdownItem<T>> {
     @Input() nestedDropdownPosition: string = 'right-top';
 
     /**
-     * We use [clrDisabled] instead of [disabled] to show that a dropdown item is disabled. However, it doesn't prevent the item from
-     * getting activated when it is clicked. So, we store the call back here and call it in {@link #onItemClicked} when it is not disabled
-     * @param callBackFunc The call back to be called when a dropdown item id clicked
+     * Argument for {@link ActionItem.handler} and {@link ActionItem.disabled} methods. Please refer to
+     * {@link ActionMenuComponent.selectedEntities}
      */
-    @Input() set onItemClickedCb(callBackFunc: (item: T) => void) {
-        this._onItemClickedCb = callBackFunc;
-    }
-    get onItemClickedCb(): (item: T) => void {
-        return this._onItemClickedCb;
-    }
-
-    /**
-     * Method to calculate disabled state of an item
-     */
-    @Input() isItemDisabledCb: (item: T) => boolean;
+    @Input() selectedEntities: unknown[];
 
     /**
      * Used for displaying different button contents in the root dropdown trigger button vs nested dropdown trigger button
@@ -183,11 +137,11 @@ export class DropdownComponent<T extends DropdownItem<T>> {
      * List of nested dropdown children that belong to this dropdown. Used to close when a different child in this menu list is
      * hovered over
      */
-    @ViewChildren(DropdownComponent) vcdDropdownChildren: QueryList<DropdownComponent<T>>;
+    @ViewChildren(DropdownComponent) vcdDropdownChildren: QueryList<DropdownComponent>;
 
     private hideTimeout: number;
 
-    private flattenNestedItemsWithSingleChild(items: T[]): T[] {
+    private flattenNestedItemsWithSingleChild(items: ActionItem<unknown, unknown>[]): ActionItem<unknown, unknown>[] {
         items.forEach((item) => {
             // Flatten out the dropdowns with single children at each level of dropdown
             this.flattenItemsWithSingleChild(items);
@@ -199,7 +153,7 @@ export class DropdownComponent<T extends DropdownItem<T>> {
         return items;
     }
 
-    private flattenItemsWithSingleChild(items: T[]): void {
+    private flattenItemsWithSingleChild(items: ActionItem<unknown, unknown>[]): void {
         const singleChildItemIndices: number[] = [];
         items.forEach((item, index) => {
             // Collect the indices of single child items
@@ -215,16 +169,6 @@ export class DropdownComponent<T extends DropdownItem<T>> {
     }
 
     /**
-     * Dropdown item click handler
-     */
-    onItemClicked(item: T): void {
-        if (this.isItemDisabledCb && this.isItemDisabledCb(item)) {
-            return;
-        }
-        this._onItemClickedCb(item);
-    }
-
-    /**
      * To check in the HTML, if a dropdown item of separator type should be used to add a separator item in the dropdown
      * This is required because,
      * 1. As each item decides its own availability, some groups may become empty and we collapse separators before them
@@ -232,7 +176,7 @@ export class DropdownComponent<T extends DropdownItem<T>> {
      * @param index Position of the dropdown item in the dropdown list
      * @param currentItem The current item in the list that is being iterated over
      */
-    shouldRenderAsSeparator(index: number, currentItem: DropdownItem<T>): boolean {
+    shouldRenderAsSeparator(index: number, currentItem: ActionItem<unknown, unknown>): boolean {
         const nextItem = this.items[index + 1];
         return index > 0 && currentItem.isSeparator && !!nextItem && !nextItem.isSeparator;
     }
@@ -284,5 +228,25 @@ export class DropdownComponent<T extends DropdownItem<T>> {
         this.vcdDropdownChildren
             .filter((vcdDropdown) => vcdDropdown.clrDropdown.toggleService.open)
             .forEach((vcdDropdown) => (vcdDropdown.clrDropdown.toggleService.open = false));
+    }
+
+    /**
+     * We use [clrDisabled] instead of [disabled] to show that a dropdown item is disabled. However, it doesn't prevent the item from
+     * getting activated when it is clicked. So, we make sure the action is not disabled before clicking it
+     */
+    onItemClicked(item: ActionItem<any, any>): void {
+        if (this.isItemDisabled(item)) {
+            return;
+        }
+        if (item.handler) {
+            item.handler(this.selectedEntities, item.handlerData);
+        }
+    }
+
+    /**
+     * To visually disable a displayed action
+     */
+    isItemDisabled(item: ActionItem<any, any>): boolean {
+        return (CommonUtil.isFunction(item.disabled) ? item.disabled(this.selectedEntities) : item.disabled) as boolean;
     }
 }
