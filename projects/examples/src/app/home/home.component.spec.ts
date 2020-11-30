4b37bbec76b3d0b5c34e5e95de7a402a7905a08d
mpatrice <mpatrice@vmware.com>
2020-11-30 16:46:43 -0500

    removed xdescribe Signed-off-by: Ria Mahoney <mpatrice@vmware.com>

--- projects/examples/src/app/home/home.component.spec.ts
+++ projects/examples/src/app/home/home.component.spec.ts
@@ -7,7 +7,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 
-xdescribe('HomeComponent', () => {
+describe('HomeComponent', () => {
     let component: HomeComponent;
     let fixture: ComponentFixture<HomeComponent>;
 
