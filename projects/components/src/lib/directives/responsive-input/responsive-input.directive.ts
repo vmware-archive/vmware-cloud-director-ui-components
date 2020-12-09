584acc9043944def9091bca38e66bf9329522ed2
yumengwu <49406822+yumengwu@users.noreply.github.com>
2020-12-09 15:16:06 -0500

    Update responsive input to increase label's width when container's width is small (#272)
    Signed-off-by: yumengwu <yumengwu95@gmail.com>

--- projects/components/src/lib/directives/responsive-input/responsive-input.directive.ts
+++ projects/components/src/lib/directives/responsive-input/responsive-input.directive.ts
@@ -3,29 +3,45 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-import { AfterViewInit, Directive, ElementRef, HostBinding } from '@angular/core';
+import { AfterViewChecked, Directive, ElementRef, HostBinding } from '@angular/core';
+
+declare type ColSize = '2' | '4' | '8' | '10';
 
 /**
- * Adds Clarity grid classes to form controls so that labels and inputs are on separate lines and to control the label width
- * for large screens.
+ * Adds Clarity grid classes to form controls based on the host's width so that labels and inputs are on separate
+ * lines and to control the label width for large screens.
  *
  * Centralizes CSS classes to be used to enforce a common look and feel throughout the application
  */
 @Directive({
     selector: '.clr-form-control[vcdResponsiveInput]',
 })
-export class ResponsiveInputDirective implements AfterViewInit {
+export class ResponsiveInputDirective implements AfterViewChecked {
+    private elementWidth = 0;
+
+    private breakPoint = 768;
+
     @HostBinding('class.clr-row') public clrGridRow = true;
     constructor(private el: ElementRef<HTMLElement>) {}
 
-    ngAfterViewInit(): void {
-        this.applyClasses('label', '2');
-        this.applyClasses('container', '10');
+    ngAfterViewChecked(): void {
+        if (this.elementWidth !== 0) {
+            return;
+        }
+        const newElementWidth = this.el.nativeElement.offsetWidth;
+        if (newElementWidth) {
+            this.elementWidth = newElementWidth;
+            const [labelWidth, containerWidth]: ColSize[] =
+                this.elementWidth < this.breakPoint ? ['4', '8'] : ['2', '10'];
+            this.applyClasses('label', labelWidth);
+            this.applyClasses('container', containerWidth);
+        }
     }
 
-    private applyClasses(className: 'label' | 'container', mdSize: '2' | '10'): void {
+    private applyClasses(className: 'label' | 'container', mdSize: ColSize): void {
         const el = this.el.nativeElement.querySelector(`:scope > .clr-control-${className}`);
         if (el) {
+            el.classList.remove('clr-col-12', 'clr-col-md-2', 'clr-col-md-4', 'clr-col-md-8', 'clr-col-md-10');
             el.classList.add('clr-col-12', `clr-col-md-${mdSize}`);
         }
     }
