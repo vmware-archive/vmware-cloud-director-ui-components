94f710c619e2b4aaf5c66f1c78167dbae92668f9
ps37 <prudhvi.af121@gmail.com>
2021-01-26 13:52:50 -0500

    Change the publish jobs to publish @latest packages on Git release tags
    Signed-off-by: ps37 <prudhvi.af121@gmail.com>

--- .github/actions/check-release-tag/index.js
+++ .github/actions/check-release-tag/index.js
@@ -2,12 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    const libName = core.getInput('lib-name');
     // Sample value of github.context.ref is "refs/heads/gh-actions"
     const tagName = github.context.ref.split('/').pop();
     console.log(`tag-name = ${tagName}`);
-    const regex = new RegExp(`^${libName}-v[0-999].[0-999].[0-999]$`);
-    core.setOutput('isLibTag', regex.test(tagName).toString());
+    const regex = new RegExp(`^v[0-999].[0-999].[0-999]$`);
+    core.setOutput('isReleaseTag', regex.test(tagName).toString());
 } catch (error) {
     core.setFailed(error.message);
 }
