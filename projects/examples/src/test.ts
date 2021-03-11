4b62e4891ff12b84071a39a644c7a1347db96e35
Ryan Bradford <bradfordr@vmware.com>
2021-03-11 16:44:29 -0500

    Remove offensive words.

--- projects/examples/src/test.ts
+++ projects/examples/src/test.ts
@@ -10,7 +10,7 @@ import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 // Required so typescript to access karma's context
-// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle, id-blacklist, id-match
+// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle, id-match
 declare const require: any;
 
 // First, initialize the Angular testing environment.
