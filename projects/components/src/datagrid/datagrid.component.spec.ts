cf757b2267f433d37807d0eab96702515626fb36
Ryan Bradford <ryanbradford333@gmail.com>
2021-03-22 14:58:06 -0400

    Fixed datagrid infinite selection loop (#322)
    Two way binding has a problem where an event from Clarity, causes the client to send an event back, which causes an infinite loop.
    
    This change stops the propagation of the events after one iteration.
    
    Signed-off-by: Ryan Bradford <bradfordr@vmware.com>

--- projects/components/src/datagrid/datagrid.component.spec.ts
+++ projects/components/src/datagrid/datagrid.component.spec.ts
@@ -298,6 +298,25 @@ describe('DatagridComponent', () => {
                     expect(this.hostComponent.selectionChanged).toHaveBeenCalledWith(mockData);
                 });
 
+                it('only emits events when selection actually changes', async function (this: HasFinderAndGrid): Promise<
+                    void
+                > {
+                    this.hostComponent.selectionType = GridSelectionType.Multi;
+                    this.finder.detectChanges();
+                    spyOn(this.hostComponent, 'selectionChanged');
+                    this.hostComponent.datagridSelection = [mockData[0]];
+                    this.finder.detectChanges();
+                    await timeout();
+                    this.finder.detectChanges();
+                    expect(this.hostComponent.selectionChanged).toHaveBeenCalledTimes(1);
+                    await timeout();
+                    this.finder.detectChanges();
+                    expect(this.hostComponent.selectionChanged).toHaveBeenCalledTimes(1);
+                    await timeout();
+                    this.finder.detectChanges();
+                    expect(this.hostComponent.selectionChanged).toHaveBeenCalledTimes(1);
+                });
+
                 it('emits only one row when set to single selection', function (this: HasFinderAndGrid): void {
                     this.hostComponent.selectionType = GridSelectionType.Single;
                     this.finder.detectChanges();
