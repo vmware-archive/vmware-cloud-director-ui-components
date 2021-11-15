c6321333ad1ac45ff5c870076b869be1a6cbb6c2
Iveta Ilieva <iilieva@vmware.com>
2021-11-15 16:03:22 +0200

    Add support for time period unit.
    - Introduced new unit for time period.
    - Fixed bug in input with unit component - the unit text should be localized.
    - Enahced some ts doc texts.
    
    Testing Done: Unit Tests. Example app updated with time period control example functionality.
    
    Signed-off-by: Iveta Ilieva <iilieva@vmware.com>

--- projects/components/src/utils/unit/unit-formatter.spec.ts
+++ projects/components/src/utils/unit/unit-formatter.spec.ts
@@ -1,10 +1,10 @@
 /*!
- * Copyright 2020 VMware, Inc.
+ * Copyright 2020-2021 VMware, Inc.
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
 import { MessageFormatTranslationService } from '@vcd/i18n';
-import { Bytes, Hertz, Percent } from './unit';
+import { Bytes, Hertz, Percent, TimePeriod } from './unit';
 import { UnitFormatter } from './unit-formatter';
 
 describe('UnitFormatter', () => {
@@ -22,6 +22,9 @@ describe('UnitFormatter', () => {
             'vcd.cc.filesize.unit.GB': '{0} GB',
             'vcd.cc.cpu.speed.unit.MHz': '{0} MHz',
             'vcd.cc.cpu.speed.unit.GHz': '{0} GHz',
+            'vcd.cc.display.timePeriod.DAYS': '{0} Days',
+            'vcd.cc.display.timePeriod.WEEKS': '{0} Weeks',
+            'vcd.cc.display.timePeriod.YEARS': '{0} Years',
         },
         fr: {},
     };
@@ -72,6 +75,22 @@ describe('UnitFormatter', () => {
         it('display value with units in best format', () => {
             expect(unitFormatter.bestFormat(50, Percent.ZERO_TO_100, [Percent.ZERO_TO_1])).toEqual('50 %');
             expect(unitFormatter.bestFormat(0.5, Percent.ZERO_TO_1, [Percent.ZERO_TO_1])).toEqual('50 %');
+        });
+    });
+
+    describe('Time Period', () => {
+        it('display value with units', () => {
+            expect(unitFormatter.format(720, TimePeriod.HOURS, TimePeriod.DAYS)).toEqual('30 Days');
+            expect(unitFormatter.format(24, TimePeriod.MONTHS, TimePeriod.YEARS)).toEqual('2 Years');
+        });
+
+        it('display value with units in best format', () => {
+            expect(unitFormatter.bestFormat(48, TimePeriod.HOURS, [TimePeriod.HOURS, TimePeriod.DAYS])).toEqual(
+                '2 Days'
+            );
+            expect(
+                unitFormatter.bestFormat(14, TimePeriod.DAYS, [TimePeriod.DAYS, TimePeriod.WEEKS, TimePeriod.MONTHS])
+            ).toEqual('2 Weeks');
         });
     });
 });
--- projects/examples/src/components/form-input/number-with-unit-form-input.example.component.ts
+++ projects/examples/src/components/form-input/number-with-unit-form-input.example.component.ts
@@ -1,9 +1,9 @@
 /*!
- * Copyright 2020 VMware, Inc.
+ * Copyright 2020-2021 VMware, Inc.
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import {
     Bytes,
@@ -11,7 +11,9 @@ import {
     Hertz,
     NumberWithUnitsFormValidatorsFactory,
     SubscriptionTracker,
+    TimePeriod,
     Unit,
+    UnitFormatter,
 } from '@vcd/ui-components';
 
 @Component({
@@ -22,20 +24,27 @@ import {
 })
 export class NumberWithUnitFormInputExampleComponent {
     formGroup: FormGroup;
+    // CPU
     hertzOptions: Unit[] = [Hertz.Mhz, Hertz.Ghz];
     cpuSpeedFormControlValueUnit: Unit = Hertz.Mhz;
     maxCpuAllowedMhz = 2400;
-
+    // Memory
     maxMemoryAllowedMB = 10240;
     memoryOptions: Unit[] = [...Bytes.types];
     memoryFormControlValueUnit: Unit = Bytes.MB;
-
     Bytes = Bytes;
+    // Time Period
+    timePeriodOptions: Unit[] = [TimePeriod.HOURS, TimePeriod.DAYS];
+    timePeriodBaseUnit: Unit = TimePeriod.HOURS;
+    timePeriodMin: number = 1;
+    timePeriodMax: number = 720;
+
     CheckBoxStyling = CheckBoxStyling;
 
     constructor(
         fb: FormBuilder,
         numberWithUnitsFormValidators: NumberWithUnitsFormValidatorsFactory,
+        private unitFormatter: UnitFormatter,
         private subscriptionTracker: SubscriptionTracker
     ) {
         const memoryValidator = numberWithUnitsFormValidators.isInRange(
@@ -52,21 +61,37 @@ export class NumberWithUnitFormInputExampleComponent {
             this.hertzOptions
         );
 
+        const timePeriodValidator = numberWithUnitsFormValidators.isInRange(
+            this.timePeriodMin,
+            this.timePeriodMax,
+            this.timePeriodBaseUnit,
+            this.timePeriodOptions
+        );
+
         this.formGroup = fb.group({
             readonly: new FormControl(false),
             disabled: new FormControl(false),
             memory: new FormControl(1024 * 2, [Validators.required, memoryValidator]),
             cpuLimit: new FormControl(-1, [cpuValidator]),
+            timePeriodSelection: new FormControl(1, [timePeriodValidator]),
         });
 
         this.subscriptionTracker.subscribe(this.formGroup.controls.disabled.valueChanges, (value) => {
             if (value) {
                 this.formGroup.controls.cpuLimit.disable();
                 this.formGroup.controls.memory.disable();
+                this.formGroup.controls.timePeriodSelection.disable();
             } else {
                 this.formGroup.controls.cpuLimit.enable();
                 this.formGroup.controls.memory.enable();
+                this.formGroup.controls.timePeriodSelection.enable();
             }
         });
+    }
+
+    get formattedTimePeriodValue() {
+        const timePeriodValue = this.formGroup.get('timePeriodSelection').value;
+
+        return this.unitFormatter.format(timePeriodValue, this.timePeriodBaseUnit, this.timePeriodBaseUnit, 0);
     }
 }
