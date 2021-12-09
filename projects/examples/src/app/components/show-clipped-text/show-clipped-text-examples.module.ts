4e941d0e22fee88d4e1ed286aa6da90ff2964f08
Tsvetomir Tsvetanov <ttsvetanov@vmware.com>
2021-12-09 10:20:20 +0200

    Tooltip for vApp/VM name doesn't close when going to new tab. It gets stuck onscreen and can be an annoyance
    Description: the Tooltip is hidden when the component is missing and destroyed
    
    Solution: hide Tooltip on ngOnDestroy
    
    Test Steps:
    - Go to vApp/VM list and hover on name
    - Click on name
    - See the Tooltip
    
    JIRA: COR-9432
    
    Signed-off-by: Tsvetomir Tsvetanov <ttsvetanov@vmware.com>

--- projects/examples/src/app/components/show-clipped-text/show-clipped-text-examples.module.ts
+++ projects/examples/src/app/components/show-clipped-text/show-clipped-text-examples.module.ts
@@ -7,6 +7,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ShowClippedTextDirective } from '@vcd/ui-components';
 import { Documentation } from '@vmw/ng-live-docs';
+import { HideClippedTextPositioningExampleComponent } from './hide/hide-clipped-text-example.component';
+import { HideClippedTextPositioningExampleModule } from './hide/hide-clipped-text-example.module';
 import { ShowClippedTextPositioningExampleComponent } from './positioning/show-clipped-text-positioning-example.component';
 import { ShowClippedTextPositioningExampleModule } from './positioning/show-clipped-text-positioning-example.module';
 import { ShowClippedTextSizingExampleComponent } from './sizing/show-clipped-text-sizing-example.component';
@@ -27,9 +29,19 @@ Documentation.registerDocumentationEntry({
             title: 'Dynamic Positioning of tooltip',
             urlSegment: 'show-clipped-text-positioning',
         },
+        {
+            component: HideClippedTextPositioningExampleComponent,
+            title: 'Hide the tooltip when the component is destroyed',
+            urlSegment: 'hide-clipped-text',
+        },
     ],
 });
 @NgModule({
-    imports: [CommonModule, ShowClippedTextPositioningExampleModule, ShowClippedTextSizingExampleModule],
+    imports: [
+        CommonModule,
+        ShowClippedTextPositioningExampleModule,
+        ShowClippedTextSizingExampleModule,
+        HideClippedTextPositioningExampleModule,
+    ],
 })
 export class ShowClippedTextExamplesModule {}
