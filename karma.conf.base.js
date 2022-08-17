df02daa9fda1eb7eda854f7371fe06a56273e53c
Georgi Atanasov <georgi.atanasov147@gmail.com>
2022-08-17 15:46:23 +0300

    Fixed issues when running tests and starting
    - Added window.global = window. to polyfills.js as a fix for 'global' is not defined error
    - Changed datagrid style calculation to use 'calc'
    - Added checks to show-clipped-text.directive as 'tip.container' or 'tip.content' is not always defined
    which makes calling 'tip.container.style' or 'tip.content.style' throw an error
    - Exported additional modules and directives since Angular was complaining they weren't but they should be
    
    Signed-off-by: Georgi Atanasov <georgi.atanasov147@gmail.com>

--- karma.conf.base.js
+++ karma.conf.base.js
@@ -5,11 +5,13 @@ module.exports = function generateKarmaConf(projectName) {
             frameworks: ['jasmine', '@angular-devkit/build-angular'],
             plugins: [
                 require('karma-jasmine'),
+                require('karma-coverage'),
                 require('karma-chrome-launcher'),
                 require('karma-jasmine-html-reporter'),
                 require('karma-coverage-istanbul-reporter'),
                 require('@angular-devkit/build-angular/plugins/karma'),
             ],
+            files: [require('path').join(__dirname, 'polyfills.js')],
             client: {
                 clearContext: false, // leave Jasmine Spec Runner output visible in browser
             },
