1eba702684c251a668c3859defe85dbc0f77e2d2
Davi Barreto <dbarreto@vmware.com>
2022-12-08 19:24:49 +0200

    fix: @vcd/ui-components compiler path should point to public-api.ts
    Before this change, the build had an issue where `index.d.ts` and `public.api.ts` would both point to each other and export no content, thus breaking the package.
    
    This change fixes it by moving the exported values from `index.ts` into `public-api.ts`, and making it the lib's entryFile.
    
    Signed-off-by: Davi Barreto <dbarreto@vmware.com>

--- projects/components/src/public-api.ts
+++ projects/components/src/public-api.ts
@@ -1,6 +1,22 @@
 /*!
- * Copyright 2019 VMware, Inc.
+ * Copyright 2022 VMware, Inc.
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
-export * from './index';
+export * from './action-menu/index';
+export * from './action-search-provider/index';
+export * from './common/activity-reporter/index';
+export * from './common/error/index';
+export * from './common/interfaces/index';
+export * from './common/loading/index';
+export * from './common/subscription/index';
+export * from './components.module';
+export * from './data-exporter/index';
+export * from './datagrid/index';
+export * from './dropdown/index';
+export * from './form/index';
+export * from './formly/vcd/index';
+export * from './lib/directives/index';
+export * from './quick-search/index';
+export * from './sharing-modal/index';
+export * from './utils/index';
