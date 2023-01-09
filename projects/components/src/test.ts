ec11da2e7960b26b7359f22ee08201ed5b1501ca
Juan Mendes <mendesjuan@gmail.com>
2023-01-09 13:52:20 -0500

    Automated changes by NX migration to ng15
    Signed-off-by: Juan Mendes <mendesjuan@gmail.com>

--- projects/components/src/test.ts
+++ projects/components/src/test.ts
@@ -18,15 +18,7 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
-// Required so typescript to access karma's context
-// eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle, id-match
-declare const require: any;
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
     teardown: { destroyAfterEach: false },
 });
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
